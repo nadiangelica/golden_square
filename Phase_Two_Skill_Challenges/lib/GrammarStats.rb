2899b89e21435bb58fbfd4f76cadfb90623e4225
nadiaAngelicaKochilas <nadia.kochilas@gmail.com>
2022-12-14 17:33:12 +0000

    Pair Programming Nadia-Thuy

--- Phase_Two_Skill_Challenges/lib/GrammarStats.rb
+++ Phase_Two_Skill_Challenges/lib/GrammarStats.rb
@@ -1,6 +1,6 @@
 class GrammarStats
   def initialize
-    # ...
+    @text_passed = []
   end
 
   def check(text) # text is a string
@@ -8,15 +8,29 @@ class GrammarStats
     # and ends with a sentence-ending punctuation mark.
     # appropriate_sentence_ending = ".!?"
     # if text[0] == text[0].capitalize && text[-1] =~ /[.?!]/
+    if text.empty? 
+      raise "No text provided!" 
+    end
+  
     if text[0] =~ /[A-Z]/ && text[-1] =~ /[.?!]/
       return true
     else
       return false
     end
+
+     @text_passed << text
   end
 
   def percentage_good
     # Returns as an integer the percentage of texts checked so far that passed
     # the check defined in the `check` method. The number 55 represents 55%.
+    return @text_passed.length
   end
 end
+# sentence = GrammarStats.new
+# print sentence.check("Hello world!")
+# print sentence.check("Hello world!")
+# print sentence.check("Hello world!")
+# print sentence.check("Hello world!")
+# print sentence.check("Hello world!")
+# print sentence.check("Hello world!")
--- Phase_Two_Skill_Challenges/spec/GrammarStats_spec.rb
+++ Phase_Two_Skill_Challenges/spec/GrammarStats_spec.rb
@@ -12,4 +12,27 @@ RSpec.describe GrammarStats do
     result = sentence.check("Hello world!")
     expect(result). to eq true
   end
+
+  it "fails" do
+    sentence = GrammarStats.new
+    expect{sentence.check("")}. to raise_error "No text provided!"
+end
+
+  context "Testing percentage_good method" do
+    it "returns an integer" do
+      sentence = GrammarStats.new
+      sentence.check("One two three. " * 5 + "one two three. " * 5 )
+      result = sentence.percentage_good
+      expect(result). to eq 5 
+    end
+
+    # it "returns an integer when two checks added" do
+    #   sentence = GrammarStats.new
+    #   sentence.check("One two three.")
+    #   sentence.check("Four five six?")
+    #   result = sentence.percentage_good
+    #   expect(result). to eq 2
+    # end
+
+  end
 end
